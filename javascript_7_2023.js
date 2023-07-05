4da3c86781841639b1f660f61184616128fa7d3b
shoya <shoyamoco021729@gmail.com>
2023-07-05 14:33:28 +0000

    フロントエンド04課題

---
+++ javascript_7_2023.js
@@ -0,0 +1,37 @@
+//フロントエンド02課題
+console.log('永井翔也'); // コンソール画面に「永井翔也」と出力する
+            
+//フロントエンド03課題
+let inputValue = prompt();
+let x = inputValue * 1.1;
+alert(x);
+
+//フロントエンド04課題
+let mon = 11;
+if (mon === 1) {
+    console.log('January'); 
+} else if (mon === 2) {
+    console.log('February'); 
+} else if (mon === 3) {
+    console.log('March'); 
+} else if (mon === 4) {
+    console.log('April'); 
+} else if (mon === 5) {
+    console.log('May'); 
+} else if (mon === 6) {
+    console.log('June'); 
+} else if (mon === 7) {
+    console.log('July'); 
+} else if (mon === 8) {
+    console.log('August'); 
+} else if (mon === 9) {
+    console.log('September');
+} else if (mon === 10) {
+    console.log('October'); 
+} else if (mon === 11) {
+    console.log('November'); 
+} else if (mon === 12) {
+    console.log('December'); 
+} else {
+    console.log('default'); 
+}
