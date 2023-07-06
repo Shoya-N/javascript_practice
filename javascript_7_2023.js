e43f4b3aa248416db9d04b36a69f66cb21185dd7
shoya <shoyamoco021729@gmail.com>
2023-07-06 12:41:50 +0000

    フロントエンド06課題

--- javascript_7_2023.js
+++ javascript_7_2023.js
@@ -44,3 +44,11 @@ for (let i = 10; i > 0; i--) {
     }
     console.log(s);
 }
+
+//フロントエンド06課題
+let fruit = ['apricot','strawberry'];
+fruit.unshift('pear');
+fruit.push('grape');
+fruit[3] = 'lime';
+
+console.log(fruit);
