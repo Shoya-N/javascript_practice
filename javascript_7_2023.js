a33712c1bce36d14ff705501619b82458e6bc73a
shoya <shoyamoco021729@gmail.com>
2023-07-06 09:32:43 +0000

    フロントエンド05課題

--- javascript_7_2023.js
+++ javascript_7_2023.js
@@ -35,3 +35,12 @@ if (mon === 1) {
 } else {
     console.log('default'); 
 }
+
+//フロントエンド05課題
+for (let i = 10; i > 0; i--) {
+    let s = '(' + i + '):';
+    for (let j = 0; j < i; j++) {
+        s = s + '*';
+    }
+    console.log(s);
+}
