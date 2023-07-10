c4e6118290c0b8287bd7bd2db630edff981aae1c
shoya <shoyamoco021729@gmail.com>
2023-07-10 08:34:45 +0000

    フロントエンド09課題

--- javascript_7_2023.js
+++ javascript_7_2023.js
@@ -84,3 +84,18 @@ var add = function(p1, p2){
     return p;
 }
 console.log(add(4, 5));
+
+
+//フロントエンド09課題
+//3の倍数
+let a = [...Array(100)].map((_, i) => i + 1)//[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+let b3 = a.filter(function(e) {
+    return e % 3 == 0;
+});
+console.log(b3);
+
+//5の倍数
+let b5 = a.filter(function(e) {
+    return e % 5 == 0;
+});
+console.log(b5);
