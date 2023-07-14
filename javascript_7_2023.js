b514f77182dd68cbc7add81961c580bc4bf68874
shoya <shoyamoco021729@gmail.com>
2023-07-14 12:55:31 +0000

    フロントエンド15課題

--- javascript_7_2023.js
+++ javascript_7_2023.js
@@ -2,10 +2,12 @@
 console.log('永井翔也'); // コンソール画面に「永井翔也」と出力する
 
 
+
 //フロントエンド03課題
 let inputValue = prompt();
 let x = inputValue * 1.1;
 alert(x);
+
 
 
 //フロントエンド04課題
@@ -39,6 +41,7 @@ if (mon === 1) {
 }
 
 
+
 //フロントエンド05課題
 for (let i = 10; i > 0; i--) {
     let s = '(' + i + '):';
@@ -49,6 +52,7 @@ for (let i = 10; i > 0; i--) {
 }
 
 
+
 //フロントエンド06課題
 let fruit = ['apricot','strawberry'];
 fruit.unshift('pear');
@@ -56,6 +60,7 @@ fruit.push('grape');
 fruit[3] = 'lime';
 
 console.log(fruit);
+
 
 
 //フロントエンド07課題
@@ -68,6 +73,7 @@ console.log(keys);
 
 let values = Object.values(fruits);
 console.log(values);
+
 
 
 //フロントエンド08課題
@@ -86,6 +92,7 @@ var add = function(p1, p2){
 console.log(add(4, 5));
 
 
+
 //フロントエンド09課題
 //3の倍数
 let a = [...Array(100)].map((_, i) => i + 1)//[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
@@ -99,6 +106,7 @@ let b5 = a.filter(function(e) {
     return e % 5 == 0;
 });
 console.log(b5);
+
 
 
 //フロントエンド10課題
@@ -131,6 +139,7 @@ console.log(total);
 //});
 
 
+
 //フロントエンド11課題
 console.log(document);
 let e1 = document.getElementById('p1');
@@ -141,11 +150,13 @@ let e3 = document.getElementById('p3');
 e3.id = 'p5'; 
 
 
+
 //フロントエンド12課題
 let target = document.getElementById('text01');
 target.addEventListener('blur', function() {
     target.outerHTML = '<span type="text">文章を入力してください</span>';
 });
+
 
 
 //フロントエンド14課題
@@ -157,3 +168,78 @@ try {
     console.log(e);
 }
 console.log('end');   
+
+
+
+//フロントエンド15課題
+//商品クラスの生成
+class Item {
+    //コンストラクタ
+    constructor(id, name, stock) {
+        //引数の値を使ってプロパティの初期化を行う
+        this.id = id;       // 商品ID
+        this.name = name;   // 商品名
+        this.stock = stock; // 在庫数
+    }
+
+    // メソッド
+    // Idの値を返す
+    getId() {
+        return this.id;
+    }
+
+    // nameの値を返す
+    getName() {
+        return this.name;
+    }
+
+    // stockの値を返す
+    getStock() {
+        return this.stock;
+    }
+
+    //　商品の追加
+    addStock(num) {
+        this.stock += num;
+    }
+
+    // 商品の販売
+    sale(num) {
+        if (this.stock < num) {
+            throw new Error('在庫が足りません')
+        } else {
+            this.stock -= num;
+        } 
+    }
+}
+
+class Chair extends Item {
+    constructor(id, name, stock, condition) {
+        //引数の値を使ってプロパティの初期化を行う
+        super(id, name, stock);
+        this.condition = condition;  // 商品の状態
+    }
+
+    getCondition() {
+        return this.condition;
+    }
+}
+
+// インスタンスの生成
+let chair1 = new Chair('0001', 'ダイニングチェア', 100, 'new');
+let chair2 = new Chair('0002', 'スツール', 100, 'used');
+let chair3 = new Chair('0003', 'ハイチェア', 10, 'used');
+let item4 = new Item('0004', 'デスク', 100);
+
+let itemList = [chair1, chair2, chair3, item4]
+
+itemList.forEach((e) => {
+    console.log(e);
+})
+
+//　全体の在庫数を確認
+let totalStock = 0;
+for (let i = 0; i < itemList.length; i++) {
+    totalStock += itemList[i].getStock();
+}
+console.log('全体の在庫数:' + totalStock);
